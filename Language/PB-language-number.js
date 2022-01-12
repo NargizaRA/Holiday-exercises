be0d64786ffa3e171f254f5cdbac79506112e825
Nargiza Rysmendieva <68547964+NargizaRA@users.noreply.github.com>
2022-01-12 09:41:47 +0100

    Add files via upload

---
+++ Language/PB-language-number.js
@@ -0,0 +1,13 @@
+// 1
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+console.log(introSentence + " " + age);
+
+// The `+` operator is concatenating the string and the number.
+
+// 2 
+let integer = "1005";
+console.log(parseInt(integer));
+
+let float = "10.05";
+console.log(parseFloat(float));
